aa4c3c74dd2d1bf67d02a6ea0cc5bfa656b51f62
anuj04patwal <anuj.patwal@sutara.org>
2025-08-07 16:13:33 +0530

    icon

--- src/resources/content.js
+++ src/resources/content.js
@@ -46,7 +46,7 @@ const social = [
 
 const home = {
   path: "/",
-  image: "/images/og/home.jpg",
+  image: "/images/avatar.jpg",
   label: "Home",
   title: `${person.name}'s Portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
